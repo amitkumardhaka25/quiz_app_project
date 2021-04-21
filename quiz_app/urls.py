76005b408bc85bc2220f280ee4929798f2eb9712
amitkumardhaka25 <amitkumardhaka25@gmail.com>
2021-04-21 12:15:41 +0530

    initial commit

---
+++ quiz_app/urls.py
@@ -0,0 +1,68 @@
+"""quiz_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+
+try:
+    from django.conf.urls import url
+except ImportError:
+    from django.urls import re_path as url
+
+
+from django.contrib import admin
+from django.urls import path
+from quiz.views import QuizListView, CategoriesListView, \
+    ViewQuizListByCategory, QuizUserProgressView, QuizMarkingList, \
+    QuizMarkingDetail, QuizDetailView, QuizTake
+
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^$',
+        view=QuizListView.as_view(),
+        name='quiz_index'),
+
+    url(r'^category/$',
+        view=CategoriesListView.as_view(),
+        name='quiz_category_list_all'),
+
+    url(r'^category/(?P<category_name>[\w|\W-]+)/$',
+        view=ViewQuizListByCategory.as_view(),
+        name='quiz_category_list_matching'),
+
+    url(r'^progress/$',
+        view=QuizUserProgressView.as_view(),
+        name='quiz_progress'),
+
+    url(r'^marking/$',
+        view=QuizMarkingList.as_view(),
+        name='quiz_marking'),
+
+    url(r'^marking/(?P<pk>[\d.]+)/$',
+        view=QuizMarkingDetail.as_view(),
+        name='quiz_marking_detail'),
+
+    #  passes variable 'quiz_name' to quiz_take view
+    url(r'^(?P<slug>[\w-]+)/$',
+        view=QuizDetailView.as_view(),
+        name='quiz_start_page'),
+
+    url(r'^(?P<quiz_name>[\w-]+)/take/$',
+        view=QuizTake.as_view(),
+        name='quiz_question'),
+]
+
